a9078357664509e4e5fa735047b12affacb55fec
Lucas Melo <lucasmelo4606@gmail.com>
2025-09-15 21:52:08 -0300

    config page chat-screen

--- src/app/chat-screen/chat-screen.component.ts
+++ src/app/chat-screen/chat-screen.component.ts
@@ -1,11 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+
+interface IChat{
+  chatTitle: string;
+  id: number;
+  userId: string
+}
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './chat-screen.component.html',
   styleUrl: './chat-screen.component.css'
 })
 export class ChatScreenComponent {
+
+  chats: IChat[];
+
+  constructor(private http: HttpClient) {// constroi a classe
+    //Inicialização de variaveis
+
+    this.chats = [];
+
+  }
+  ngOnInit() { //executado quando o Angula esta pronto para rodar
+
+    // Buscar dados na API
+    this.getChats();
+
+  }
+
+  async getChats() {
+    // Metodo que busca os chats da API
+
+    let response = await this.http.get("https://senai-gpt-api.azurewebsites.net/chats",
+      {
+        headers: {
+         "Authorization": "Bearer "+ localStorage.getItem("meuToken")
+
+        }
+      }
+    ).toPromise();
+      if(response){
+        this.chats = response as [];
+      
+      }else{
+        console.log("erro ao buscar ")
+      }
+
+    
+  }
 
 }
