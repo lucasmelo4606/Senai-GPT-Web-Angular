2c69b5a280b839adfcaccd212edb813ee0d70a90
Lucas Melo <lucasmelo4606@gmail.com>
2025-09-16 21:46:19 -0300

    config page chat-screen

--- src/app/chat-screen/chat-screen.component.ts
+++ src/app/chat-screen/chat-screen.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { HttpClient,  } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 
 interface IChat{
@@ -8,21 +9,32 @@ interface IChat{
   id: number;
   userId: string
 }
+interface IMessage{
+  chatId: number;
+  id: number;
+  text: string;
+  userId: string;
+}
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [HttpClientModule, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './chat-screen.component.html',
   styleUrl: './chat-screen.component.css'
 })
 export class ChatScreenComponent {
 
   chats: IChat[];
+  chatSelecionado: IChat;
+  messages: IMessage [];
 
-  constructor(private http: HttpClient) {// constroi a classe
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {// constroi a classe
     //Inicialização de variaveis
 
     this.chats = [];
+    this.chatSelecionado = null!;
+    this.messages =[];
+
 
   }
   ngOnInit() { //executado quando o Angula esta pronto para rodar
@@ -35,14 +47,14 @@ export class ChatScreenComponent {
   async getChats() {
     // Metodo que busca os chats da API
 
-    let response = await this.http.get("https://senai-gpt-api.azurewebsites.net/chats",
+    let response = await firstValueFrom(this.http.get("https://senai-gpt-api.azurewebsites.net/chats",
       {
         headers: {
          "Authorization": "Bearer "+ localStorage.getItem("meuToken")
 
         }
       }
-    ).toPromise();
+    ))
       if(response){
         this.chats = response as [];
       
@@ -50,6 +62,23 @@ export class ChatScreenComponent {
         console.log("erro ao buscar ")
       }
 
+    this.cd.detectChanges();
+  }
+
+  async onChatClick(chatClicado: IChat){
+    console.log("chat clicado", chatClicado)
+    this.chatSelecionado= chatClicado;
+
+    //lógica para buscar as mensagens
+    let response = await firstValueFrom(this.http.get("https://senai-gpt-api.azurewebsites.net/messages?chatId=" + chatClicado.id,
+      {
+        headers: {
+         "Authorization": "Bearer "+ localStorage.getItem("meuToken")
+
+        }
+      }));
+      console.log("MESSAGENS", response)
+      this.messages = response as IMessage[];
     
   }
 
